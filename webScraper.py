3cad709961dbe0257d7df65ad45dad1dbd8f5144
sinderpl <18658841+sinderpl@users.noreply.github.com>
2017-07-27 20:27:49 +0100

    Now getting basic text data back

--- webScraper.py
+++ webScraper.py
@@ -2,7 +2,23 @@
 import urllib2
 #Import the regex matching pattern
 import re
+#HTML parser
+from HTMLParser import HTMLParser
 
-data = urllib2.urlopen("http://testing-ground.scraping.pro/")
+
+
+
+
+# create a subclass and override the handler methods
+class MyHTMLParser(HTMLParser):
+    def handle_data(self, data):
+        print "Encountered some data  :", data
+
+# instantiate the parser and fed it some HTML
+parser = MyHTMLParser()
+data = urllib2.urlopen("http://testing-ground.scraping.pro/blocks")
+
+
 for line in data:
-    print line+"\n ----------------------"
+    parser.feed(line)
+   #print line+"\n ----------------------"

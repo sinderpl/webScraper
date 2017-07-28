57a4347693e6fa8f8f3cb9ffa317bb702a74b780
sinderpl <18658841+sinderpl@users.noreply.github.com>
2017-07-28 11:38:34 +0100

    adding words to a dictionary and counting the occurence rate

--- webScraper.py
+++ webScraper.py
@@ -8,22 +8,37 @@ from HTMLParser import HTMLParser
 import glob
 
 
-words = dict({})
-aplhabet = dict({"a" : 0 , "b" : 0, "c": 0 ,"d" : 0, "e" : 0, "f" : 0, "g" : 0,
+wordDictionary = dict({})
+aplhabetDictionary = dict({"a" : 0 , "b" : 0, "c": 0 ,"d" : 0, "e" : 0, "f" : 0, "g" : 0,
 "h" : 0, "i" : 0 , "j" : 0, "k" : 0, "l" : 0 , "m" : 0, "n" : 0, "o" : 0, "p": 0,
 "q": 0, "r" : 0, "s" : 0, "t" : 0, "u" : 0, "v": 0, "w" : 0, "x" : 0,
  "y" : 0, "z" : 0})
 longestWord = ""
 commonLetter = "";
 
+pattern = re.compile(r'([a-zA-Z]+)')
+
+def addWordsFromLine( words):
+    for word in words:
+        if word in wordDictionary:
+            wordDictionary[word] += 1
+            #my_dict.get('corse') and my_dict.update({'corse': 'my definition'})
+        else:
+            wordDictionary[word] = 1
+            #wordDictionary
+
 # create a subclass and override the handler methods
 class MyHTMLParser(HTMLParser):
+
     def handle_data(self, data):
-        #print "Encountered some data  :", data
-        pattern = re.compile(r'([a-zA-Z]+)') #r'([A-Z]+)')
+        #F
         f = re.findall(pattern,data)
         if f:
-            print "New data: ", f
+            #print "New data: ", f
+            addWordsFromLine(f)
+
+
+
 
 # instantiate the parser and fed it some HTML
 parser = MyHTMLParser()
@@ -36,3 +51,5 @@ data = urllib2.urlopen("http://testing-ground.scraping.pro/blocks")
 for line in data:
     parser.feed(line)
    #print line+"\n ----------------------"
+
+print wordDictionary

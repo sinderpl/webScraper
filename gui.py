fa05ce73a9611307d5b5db0345da818fa05051ca
sinderpl <18658841+sinderpl@users.noreply.github.com>
2017-08-04 14:37:37 +0100

    Gui now has link entry area

--- gui.py
+++ gui.py
@@ -1,17 +1,50 @@
-from Tkinter import *
+import Tkinter
 import webScraper
-
-webScraper.main()
-root = Tk()
-root.title("Web Scraper")
-listb = Listbox(root)
+from Tkinter import *
 
 
-li = 'Carl Patrick Lindsay Helmut Chris Gwen'.split()
 
-for word in webScraper.getWordDictionary():
-    listb.insert(END,word)
 
-listb.delete(0, END)
-listb.pack()
-root.mainloop()
+class simpleapp_tk(Tkinter.Tk):
+
+    def __init__(self,parent):
+        Tkinter.Tk.__init__(self,parent)
+        self.parent = parent
+        self.initialize()
+
+    def initialize(self):
+        #Set up the grid and window size
+        self.grid()
+        self.geometry("800x800")
+
+        #Set up the link entry area
+        self.labelAdress = Label(self,text = "Please enter the link you wish to scrape: ")
+        self.labelAdress.grid()
+        self.labelAdress.grid(column=0,row=0,sticky='EW')
+        self.linkVariable = Tkinter.StringVar()
+        self.linkVariable.set(u"http://testing-ground.scraping.pro")
+        self.linkEntry = Tkinter.Entry(self,textvariable=self.linkVariable)
+        self.linkEntry.grid(column=0,row=1,sticky='EW')
+
+
+
+
+        '''
+        listb = Listbox(root)
+
+
+        for word in webScraper.getWordDictionary():
+            tempString = "%"
+            listb.insert(END,)
+
+        #Delete the list
+        #listb.delete(0, END)
+        #listb.pack()
+        '''
+
+if __name__ == "__main__":
+    #Call the scraper
+    #webScraper.main()
+    app = simpleapp_tk(None)
+    app.title('Web Scraper')
+    app.mainloop()
